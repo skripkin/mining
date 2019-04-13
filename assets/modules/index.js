50e06f52b379506feca8fbfec0260ad686743a40
skripkin.eugene <skripkin.eugene@gmail.com>
2019-04-13 15:37:19 +0600

    Create news-slider module

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -1,1 +1,2 @@
 export { default as Dropdowns } from './dropdown';
+export { default as NewSlider } from './news-slider';
---
+++ assets/modules/news-slider/index.js
@@ -0,0 +1,62 @@
+import { tns } from 'tiny-slider/src/tiny-slider';
+
+class NewSlider {
+    constructor() {
+        this._newSlider = document.querySelector('.news-slider');
+
+        if (!this._newSlider) {
+            return this.destructor();
+        }
+
+        const container = this._newSlider.querySelector('.news-slider__content');
+
+        this._slider = tns({
+            container,
+            autoplay: true,
+            speed: 1000,
+            autoplayTimeout: 7000,
+            autoplayHoverPause: true,
+            controlsText: [
+                '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--prev"></span>',
+                '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--next"></span>',
+            ],
+            items: 2,
+            mouseDrag: true,
+            freezable: false,
+            responsive: {
+                1200: {
+                    controlsText: [
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--prev"></span>',
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--next"></span>',
+                    ],
+                    items: 3.4
+                },
+                1100: {
+                    controlsText: [
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--prev"></span>',
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--next"></span>',
+                    ],
+                    items: 2.8
+                },
+                985: {
+                    controlsText: [
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--prev"></span>',
+                        '<span class="news-slider__gallery-arrow arrow news-slider__gallery-arrow--next"></span>',
+                    ],
+                    items: 2.4
+                }
+            }
+        });
+    }
+
+    destructor() {
+        if (this._slider) {
+            this._slider.destroy();
+            this._slider = null;
+        }
+
+        this._newSlider = null;
+    }
+}
+
+export default new NewSlider();
